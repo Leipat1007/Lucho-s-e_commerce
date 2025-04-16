59505e6f8c79381837c6b15f3376fe63e6d6a322
Aldair <aldairsanchez0903@gmail.com>
2025-04-16 18:51:26 -0500

    Implementación de las animaciones

--- assets/Js/contact.js
+++ assets/Js/contact.js
@@ -2,11 +2,16 @@ const btnRedes = document.getElementById("btnRedes");
 const redesColapsar = document.getElementById("redesColapsar");
 btnRedes.addEventListener("click",  () => {
     if(redesColapsar.classList.contains('disabled')){
-        redesColapsar.classList.remove('disabled');
+        redesColapsar.classList.remove('disabled', 'slide-Up');
+        redesColapsar.classList.add('slide-down');
         btnRedes.innerHTML = 'Redes sociales <i class="bi bi-chevron-up"></i>';
     }
     else{
-        redesColapsar.classList.add('disabled');
+        redesColapsar.classList.remove('slide-down');
+        redesColapsar.classList.add('slide-Up');
+        redesColapsar.addEventListener('animationend', () => {
+            redesColapsar.classList.add('disabled');
+        }, {once: true});
         btnRedes.innerHTML = 'Redes sociales <i class="bi bi-chevron-down"></i>';
     }
 });
@@ -14,11 +19,16 @@ const btnContacto = document.getElementById("btnContacto");
 const contactoColapsar = document.getElementById("contactoColapsar");
 btnContacto.addEventListener("click",  () => {
     if(contactoColapsar.classList.contains('disabled')){
-        contactoColapsar.classList.remove('disabled');
+        contactoColapsar.classList.remove('disabled', 'slide-Up');
+        contactoColapsar.classList.add('slide-down');
         btnContacto.innerHTML = 'Contactanos <i class="bi bi-chevron-up"></i>';
     }
     else{
-        contactoColapsar.classList.add('disabled');
+        contactoColapsar.classList.remove('slide-down');
+        contactoColapsar.classList.add('slide-Up');
+        contactoColapsar.addEventListener('animationend', () => {
+            contactoColapsar.classList.add('disabled');
+        }, {once: true});
         btnContacto.innerHTML = 'Contactanos <i class="bi bi-chevron-down"></i>';
     }
 });

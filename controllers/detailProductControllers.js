884f8d5ae9ad2150282c5fbcd0f197de0cc4f607
Aldair <aldairsanchez0903@gmail.com>
2025-04-19 01:04:30 -0500

    Refactor addCart function to accept product object and update event listener for adding products to cart

--- controllers/detailProductControllers.js
+++ controllers/detailProductControllers.js
@@ -17,17 +17,18 @@ const productCard = (image, nombre, precio, descripcion, id) => {
     return card;
 };
 //Función para guardar el id del producto en el localStorage
-const addCart = (id) => {
+const addCart = (producto) => {
     // Obtener el carrito del localStorage o inicializarlo como un array vacío
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     // Verificar si el producto ya está en el carrito
+    const exists = cart.some((item) => item.id === producto.id);
     // Si no está en el carrito, lo agrego
-    if (!cart.includes(id)) {
-        cart.push(id); // Agregar el id del producto al carrito
+    if (!exists) {
+        cart.push(producto); // Agregar el producto al carrito
         // Guardar el carrito actualizado en el localStorage
         localStorage.setItem('cart', JSON.stringify(cart));
         alert('Producto añadido al carrito');
-    }else {
+    } else {
         alert('El producto ya está en el carrito');
     }
 };
@@ -36,15 +37,25 @@ document.addEventListener('DOMContentLoaded', async () => {
         const url = new URL(window.location);
         const id = url.searchParams.get('id');
         productService.productoId(id).then((producto) => {
-            const { image, nombre, precio, descripcion, id } = producto;
+            const { image, nombre, precio, descripcion, stock, id } = producto;
             detailProductCard.appendChild(productCard(image, nombre, precio, descripcion, id));
+            // Agrego el evento al botón de agregar al carrito
             const addCartButton = document.querySelector('.addCart');
             if (addCartButton) { //Valido que el botón exista
                 // Agrego el evento al botón de agregar al carrito
                 addCartButton.addEventListener('click', (e) => {
-                    const id = e.target.dataset.id; // Obtengo el id del producto
-                    // Agrego el id al carrito
-                    addCart(id);
+                    // Prevenir el comportamiento por defecto del botón
+                    e.preventDefault();
+                    // creamos el objeto del producto a agregar al carrito
+                    const productoCart = {
+                        id,
+                        nombre,
+                        precio,
+                        image,
+                        stock,
+                        cantidad: 1 // Inicializo la cantidad en 1
+                    };
+                    addCart(productoCart); // Llamo a la función para agregar el producto al carrito
                 });
             }
     }).catch((error) => {   
